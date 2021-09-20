d2c9565538379a039478c7190e65647a23222044
tim860126 <45734181+tim860126@users.noreply.github.com>
2021-09-21 00:35:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import investpy
 
 def get_search(form,to):
-    df = investpy.get_stock_historical_data(stock='AAPL',
+    jsonda = investpy.get_stock_historical_data(stock='AAPL',
                                         country='United States', 
                                         from_date=form, 
                                         to_date=to,
                                         as_json=True)
 
-    return df
+    return jsonda
 
 form="01/01/2021"
 to="27/09/2021"
